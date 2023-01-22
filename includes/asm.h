6dd9fa96bc409c6ca706aadf6eeae5dee019053e
azajay08 <aaron.p.j@hotmail.com>
2023-01-22 18:11:19 +0200

    added input check to asm

--- includes/asm.h
+++ includes/asm.h
@@ -6,12 +6,15 @@
 /*   By: ajones <ajones@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 15:12:53 by ajones            #+#    #+#             */
-/*   Updated: 2023/01/20 17:47:54 by ajones           ###   ########.fr       */
+/*   Updated: 2023/01/22 18:10:47 by ajones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ASM_H
 # define ASM_H
+
+# define USAGE "Usage: ./asm [filename.s]\n"
+# define EXT_S "Invalid file! Input file must have [.s] extension\n"
 
 # include "../libft/includes/libft.h"
 
--- srcs/asm/main.c
+++ srcs/asm/main.c
@@ -6,14 +6,31 @@
 /*   By: ajones <ajones@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 03:17:36 by ajones            #+#    #+#             */
-/*   Updated: 2023/01/20 17:09:53 by ajones           ###   ########.fr       */
+/*   Updated: 2023/01/22 18:10:42 by ajones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
+int	verify_filename(char *filename)
+{
+	if (!ft_strequ(ft_strrchr(filename, '.'), ".s"))
+	{
+		ft_putstr(EXT_S);
+		return (0);
+	}
+	return (1);
+}
+
+void	error_exit(char *exit_str)
+{
+	ft_putstr(exit_str);
+	exit(1);
+}
+
 int	main(int argc, char **argv)
 {
-	
+	if (argc != 2 || !verify_filename(argv[1]))
+		error_exit(USAGE);
 	return (0);
 }
