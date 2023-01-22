d625b60483d7bf507ac3040fd05a8bd4ace297d7
azajay08 <aaron.p.j@hotmail.com>
2023-01-22 19:30:30 +0200

    added filename check

--- includes/asm.h
+++ includes/asm.h
@@ -6,7 +6,7 @@
 /*   By: ajones <ajones@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 15:12:53 by ajones            #+#    #+#             */
-/*   Updated: 2023/01/22 18:10:47 by ajones           ###   ########.fr       */
+/*   Updated: 2023/01/22 19:27:48 by ajones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,22 @@
 
 # define USAGE "Usage: ./asm [filename.s]\n"
 # define EXT_S "Invalid file! Input file must have [.s] extension\n"
+# define ASSEM "ERROR! Memory allocation of t_asm failed!\n"
 
 # include "../libft/includes/libft.h"
 
 typedef struct	s_asm
 {
 	char		*name;
+	char		*filename;
 }				t_asm;
 
-int	main(int argc, char **argv);
+int		main(int argc, char **argv);
+
+/*
+	Initialising
+*/
+
+void	init_asm(t_asm *assem, char *file_input);
 
 #endif
--- srcs/asm/main.c
+++ srcs/asm/main.c
@@ -6,7 +6,7 @@
 /*   By: ajones <ajones@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 03:17:36 by ajones            #+#    #+#             */
-/*   Updated: 2023/01/22 18:10:42 by ajones           ###   ########.fr       */
+/*   Updated: 2023/01/22 19:26:40 by ajones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,13 @@ void	error_exit(char *exit_str)
 
 int	main(int argc, char **argv)
 {
+	t_asm	*assem;
+	
 	if (argc != 2 || !verify_filename(argv[1]))
 		error_exit(USAGE);
+	assem = (t_asm *)malloc(sizeof(t_asm));
+	if (!assem)
+		error_exit(ASSEM);
+	init_asm(assem, argv[1]);
 	return (0);
 }
