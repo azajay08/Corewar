4a4db00a35e1dfbc152eb1067261c5b7c2713b4e
azajay08 <aaron.p.j@hotmail.com>
2023-02-04 21:41:27 +0200

    made small change the verify file

--- srcs/asm/errors.c
+++ srcs/asm/errors.c
@@ -1,36 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   errors.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ajones <ajones@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/20 03:17:36 by ajones            #+#    #+#             */
-/*   Updated: 2023/02/04 21:40:54 by ajones           ###   ########.fr       */
+/*   Created: 2023/02/04 21:40:37 by ajones            #+#    #+#             */
+/*   Updated: 2023/02/04 21:40:59 by ajones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-static void	verify_filename(char *filename)
-{
-	if (!ft_strequ(ft_strrchr(filename, '.'), ".s"))
-	{
-		ft_putstr(EXT_S);
-		error_exit(USAGE);
-	}
-}
-
-int	main(int argc, char **argv)
-{
-	t_asm	*assem;
-	
-	if (argc != 2)
-		error_exit(USAGE);
-	verify_filename(argv[1]);
-	assem = (t_asm *)malloc(sizeof(t_asm));
-	if (!assem)
-		error_exit(ASSEM);
-	init_asm(assem, argv[1]);
-	return (0);
-}
