7bc05dfa8daeea34d0aee25a6ef6f71f3699cbec
azajay08 <aaron.p.j@hotmail.com>
2023-02-06 22:33:18 +0200

    added macros for for conversion table

--- includes/asm.h
+++ includes/asm.h
@@ -6,18 +6,36 @@
 /*   By: ajones <ajones@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 15:12:53 by ajones            #+#    #+#             */
-/*   Updated: 2023/02/06 15:58:54 by ajones           ###   ########.fr       */
+/*   Updated: 2023/02/06 16:52:25 by ajones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ASM_H
 # define ASM_H
 
+# include "../libft/includes/libft.h"
+# include "op.h"
+
 # define USAGE "Usage: ./asm [filename.s]\n"
 # define EXT_S "Invalid file! Input file must have [.s] extension\n\n"
 # define ASSEM "ERROR! Memory allocation of t_asm failed!\n"
 
-# include "../libft/includes/libft.h"
+# define LIVE 1
+# define LD 2
+# define ST 3
+# define ADD 4
+# define SUB 5
+# define AND 6
+# define OR 7
+# define XOR 8
+# define ZJMP 9
+# define LDI 10
+# define STI 11
+# define FORK 12
+# define LLD 13
+# define LLDI 14
+# define LFORK 15
+# define AFF 16
 
 typedef struct	s_asm
 {
