4780a0df643fbb5ee81d788bf3fcd991193e6c85
azajay08 <aaron.p.j@hotmail.com>
2023-02-09 13:51:24 +0200

    changed champ comment

--- srcs/asm/init.c
+++ srcs/asm/init.c
@@ -6,7 +6,7 @@
 /*   By: ajones <ajones@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 19:21:12 by ajones            #+#    #+#             */
-/*   Updated: 2023/02/07 13:43:26 by ajones           ###   ########.fr       */
+/*   Updated: 2023/02/09 13:51:18 by ajones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,5 @@ void	init_asm(t_asm *assem, char *file_input)
 	assem->filename = ft_strsub(file_input, 0, len);
 	assem->filename = ft_strjoin_free1(assem->filename, ".cor");
 	assem->champ_name = NULL;
-	assem->comment = NULL;
+	assem->champ_com = NULL;
 }
