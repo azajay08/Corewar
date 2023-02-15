fca5ea5b3eef4caa651ca5da8f9723c485869d4e
azajay08 <aaron.p.j@hotmail.com>
2023-02-15 15:18:52 +0200

    cleaned up unused comments

--- srcs/asm/free.c
+++ srcs/asm/free.c
@@ -6,24 +6,11 @@
 /*   By: ajones <ajones@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 13:59:41 by ajones            #+#    #+#             */
-/*   Updated: 2023/02/13 16:49:18 by ajones           ###   ########.fr       */
+/*   Updated: 2023/02/15 15:18:31 by ajones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
-
-// static void	print_map(t_asm *assem)
-// {
-// 	t_line	*tmp;
-
-// 	tmp = assem->line;
-// 	while (tmp)
-// 	{
-// 		ft_putstr(tmp->line);
-// 		ft_putchar('\n');
-// 		tmp = tmp->next;
-// 	}
-// }
 
 void	free_line(t_asm *assem)
 {
@@ -32,7 +19,6 @@ void	free_line(t_asm *assem)
 	while (assem->line)
 	{
 		temp = assem->line;
-		// ft_printf("\nline: %s", temp->line);
 		assem->line = assem->line->next;
 		ft_strdel(&temp->line);
 		free(temp);
@@ -41,8 +27,6 @@ void	free_line(t_asm *assem)
 
 void	free_asm(t_asm *assem)
 {
-	// print_map(assem);
-	// print_map(assem);
 	if(assem->line)
 		free_line(assem);
 	if (assem->champ_name)
