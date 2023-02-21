4220de28def88bb42d5055932a7bd628536d3295
azajay08 <aaron.p.j@hotmail.com>
2023-02-22 01:47:45 +0200

    cleaning

--- includes/asm.h
+++ includes/asm.h
@@ -6,7 +6,7 @@
 /*   By: ajones <ajones@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 15:12:53 by ajones            #+#    #+#             */
-/*   Updated: 2023/02/20 16:55:07 by ajones           ###   ########.fr       */
+/*   Updated: 2023/02/22 01:47:34 by ajones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,23 +26,6 @@
 # define INV_FILE	"ERROR! Inavlid File!\n"
 # define LONG_NAME	"ERROR! Name too long\n"
 # define LONG_COM	"ERROR! Comment too long\n"
-
-# define LIVE 1
-# define LD 2
-# define ST 3
-# define ADD 4
-# define SUB 5
-# define AND 6
-# define OR 7
-# define XOR 8
-# define ZJMP 9
-# define LDI 10
-# define STI 11
-# define FORK 12
-# define LLD 13
-# define LLDI 14
-# define LFORK 15
-# define AFF 16
 
 typedef struct s_asm
 {
