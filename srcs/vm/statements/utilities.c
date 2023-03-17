46c0a45812b1335255ea859c1a09baf77df22c4a
tlahin <teemu.lahin@gmail.com>
2023-03-17 15:54:38 +0200

    deleted check_args

--- srcs/vm/statements/utilities.c
+++ srcs/vm/statements/utilities.c
@@ -24,24 +24,6 @@ int    mod_calculator(int pos)
     return (pos);
 }
 
-int8_t	check_args(t_carriage *carriage)
-{
-	int			i;
-	u_int8_t	bit_pair;
-
-	if (g_op_tab[carriage->op_code].arg_type_code == 0)
-		return (0);
-	i = 0;
-	while (i < g_op_tab[carriage->op_code].arg_num)
-	{
-		bit_pair = get_bit_pair(carriage->pos + 1, i);
-		if (bit_pair != g_op_tab[carriage->op_code].arg_type[i])
-			return (-1);
-		i++;
-	}
-	return (0);
-}
-
 void	write_n_byte(t_vm *vm, uint32_t index, uint32_t size, uint32_t data)
 {
 	index = mod_calculator(index);
