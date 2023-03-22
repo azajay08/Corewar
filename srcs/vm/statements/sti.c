f05211a9eee4d8c420fc83247d0652c0ba1c3910
Samyewel <77693164+Samyewel@users.noreply.github.com>
2023-03-22 17:15:45 +0200

    fixed a missed debug flag

--- srcs/vm/statements/sti.c
+++ srcs/vm/statements/sti.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sti.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ajones <ajones@student.42.fr>              +#+  +:+       +#+        */
+/*   By: sam <sam@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 12:47:48 by sam               #+#    #+#             */
-/*   Updated: 2023/03/22 00:57:22 by ajones           ###   ########.fr       */
+/*   Updated: 2023/03/22 17:15:15 by sam              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,6 @@ void	ft_sti(t_carriage *carriage, t_vm *vm)
 	result = mod_calculator((carriage->pos + result));
 	write_n_byte(vm, result, 3, \
 				carriage->registers[carriage->args[0].value - 1]);
-	if (DEBUG == true)
+	if (vm->b_flag)
 		ft_printf("args[0]type = |%d|  REG|%d| value is = |%d| was written to pos = |%d|", carriage->args[0].type, carriage->args[0].value, carriage->registers[15], result);
 }
